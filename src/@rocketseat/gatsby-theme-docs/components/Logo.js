2b4adb280acebdbbf13783bfdc6a31cab9236479
CoodingPenguin <unodostre@naver.com>
2020-07-26 16:48:31 +0900

    Change logo
    - 로고 픽셀 크기 수정
    - 다양한 로고 업데이트

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -3,8 +3,8 @@ import styled from "@emotion/styled";
 import logo from "../assets/penguin.svg";
 
 export default styled.div`
-  width: 160px;
-  height: 36px;
+  width: 200px;
+  height: 42px;
   background-size: contain;
   background: url(${logo}) center no-repeat;
 `;
